7272ef4740cf402affea3d70d1a3b4a4e4e0a558
z4nkomziry-hash <z4nkomziry@gmail.com>
2026-07-12 23:15:05 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,36 @@
+// ============================================
+// CONFIG.JS - زانیارییە تایبەتییەکان
+// ئەم فایلە لە GitHub شەیر مەکە!
+// ============================================
+
+const AppConfig = {
+    // زانیارییەکانی پارەدان
+    payment: {
+        fib: '+964 750 604 5491',
+        fastpay: '+964 750 604 5491',
+        usdt: 'TKUfVwnjyT2KUa9xnBreT32YLLJEwACHpc',
+        korek: '',
+        zain: '',
+        asia: '',
+        qi: '',
+        apple: '',
+        itunes: ''
+    },
+    
+    // زانیارییەکانی پەیوەندی
+    contact: {
+        whatsapp: '+9647506045491',  // بێ بۆشایی و +
+        telegram: 'z_14x',
+        email: ''
+    },
+    
+    // Admin
+    adminPassword: 'ziman2024',  // پاسۆردی خۆت دابنێ
+    
+    // Google AdSense (ئەگەر هەبێت)
+    adsense: {
+        enabled: false,
+        clientId: '',
+        slotId: ''
+    }
+};
