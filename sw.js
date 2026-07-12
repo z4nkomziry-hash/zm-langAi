218d0793f25217d085c9ce0ffa7ecbd7a51eaea1
z4nkomziry-hash <z4nkomziry@gmail.com>
2026-07-12 22:33:11 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+// ============ SERVICE WORKER ============
+const CACHE_NAME = 'ziman-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            return cached || fetch(event.request).then((response) => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, clone);
+                });
+                return response;
+            });
+        }).catch(() => {
+            return caches.match('/index.html');
+        })
+    );
+});
